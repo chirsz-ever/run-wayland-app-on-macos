1b8961fae9dab9687f379cba764cd07f4fd3171b
chirsz-ever <chirsz-ever@outlook.com>
2025-07-14 15:53:44 +0800

    add submodules and build steps

---
+++ README.md
@@ -0,0 +1,23 @@
+Build Steps:
+
+1. Prepare the repository
+
+```sh
+git clone https://github.com/chirsz-ever/run-wayland-app-on-macos.git
+cd run-wayland-app-on-macos
+git submodule update --init --filter=blob:none
+```
+
+2. Build and install to `fakeroot`
+
+```sh
+./build.sh
+```
+
+3. Run
+
+```sh
+export XDG_RUNTIME_DIR="/tmp"
+./owl/build/Owl.app/Contents/MacOS/Owl &
+./havoc/build/havoc
+```
---
+++ build.sh
@@ -0,0 +1,81 @@
+#!/usr/bin/env bash
+
+set -e
+# set -x
+
+THIS_DIR=$(realpath "$(dirname "$0")")
+INSTALL_PREFIX_DIR="${THIS_DIR}/fakeroot"
+
+PATH="${PATH}:${INSTALL_PREFIX_DIR}/bin"
+
+export CMAKE_PREFIX_PATH=$INSTALL_PREFIX_DIR
+export PKG_CONFIG_PATH="$INSTALL_PREFIX_DIR/lib/pkgconfig:$INSTALL_PREFIX_DIR/share/pkgconfig"
+
+compile_wayland() {
+    pushd wayland
+
+    meson setup \
+        --prefix "$INSTALL_PREFIX_DIR" \
+        -Dtests=false \
+        -Ddocumentation=false \
+        -Ddtd_validation=false \
+        build
+    meson compile -C build
+    meson install -C build
+
+    popd
+}
+
+compile_wayland-protocols() {
+    pushd wayland-protocols
+
+    meson setup \
+        --prefix "$INSTALL_PREFIX_DIR" \
+        -Dtests=false \
+        build
+    meson compile -C build
+    meson install -C build
+
+    popd
+}
+
+compile_owl() {
+    pushd owl
+
+    mkdir -p build && cd build
+
+    export CFLAGS;
+    CFLAGS=$(pkg-config --cflags wayland-server xkbcommon)
+    export LDFLAGS;
+    LDFLAGS=$(pkg-config --libs wayland-server xkbcommon)
+    ../configure
+    make
+
+    popd
+}
+
+compile_havoc() {
+    pushd havoc
+
+    meson setup build
+    meson compile -C build
+
+    popd
+}
+
+compile_all() {
+    compile_wayland
+    compile_wayland-protocols
+    compile_owl
+    compile_havoc
+}
+
+cd "$THIS_DIR"
+
+if [[ $# == 0 ]]; then
+    compile_all
+elif [[ $1 == "-h" || $1 == "--help" || $1 == "help" ]]; then
+    echo "Usage: ./build.sh [all|wayland|wayland-protocols|owl|havoc]"
+else
+    compile_"$1"
+fi
